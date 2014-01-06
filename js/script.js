7595a8ff3dcfe38f03341ab19e4914f1f1c59c1e
Kaung Htet Zaw <emoosx@gmail.com>
2014-01-06 19:28:12 +0630

    Fixes to suit bootstrap 3.0

--- js/script.js
+++ js/script.js
@@ -1,18 +1,46 @@
 $(document).ready(function() {
-    $('.guy-text').transition({
-        x: '20%'
-    }, 2000, 'ease');
+    $('.guy-text').animo({
+      animation: ['bounceInLeft', 'tada'],
+      duration: 2.5,
+    });
 
-    $('.girl-text').transition({
-        x: '-20%'
-    }, 2000, 'ease');
+    $('.girl-text').animo({
+      animation: ['bounceInRight', 'bounce'],
+      duration: 2.5,
+    });
 
-    $('.guy').transition({
-        x: '100'
-    }, 2000, 'ease');
+    $('.glyphicon-heart').animo("blur", {
+      amount: 3,
+      duration: 0.6,
+      focusAfter: 3.5,
+      keep: true
+    });
 
-    $('.girl').transition({
-        x: '-100'
-    }, 2000, 'ease');
+    // $('.guy-text').transition({
+        // x: '20%'
+    // }, 2000, 'ease');
+
+    // $('.girl-text').transition({
+        // x: '-20%'
+    // }, 2000, 'ease');
+
+    // $('.guy').transition({
+        // x: '100'
+    // }, 2000, 'ease');
+
+    // $('.girl').transition({
+        // x: '-100'
+    // }, 2000, 'ease');
+
+    $('#support1').animo({animation: 'bounceInLeft', duration: 1}, function() {
+      $('#support2').animo({animation: 'bounceInRight', duration: 1});
+    });
+
+    $('#support1').animo({animation: 'fadeInLeft', duration: 0.5}, function() {
+      $('#support2').animo({animation: 'fadeInUp', duration: 0.5}, function() {
+        $('#support3').animo({animation: 'fadeInRight', duration: 0.5}, function() {
+        });
+      });
+    });
 
 });
